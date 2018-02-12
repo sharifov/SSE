eb7c5eadfb7f5db5e2e4134718a0f1f9d52d0938
Sharifov <sharifov@programmer.net>
2018-02-12 18:17:52 +0400

    Change if

--- server.php
+++ server.php
@@ -4,6 +4,8 @@ header('Content-Type: text/event-stream'); //required
 
 $s = date('s');
 
-if($s & 1) print 'data: odd - '.$s."\n\n"; else print 'data: even - '.$s."\n\n";
+if($s & 1) print 'data: odd - '.$s."\n\n"; 
+
+else print 'data: even - '.$s."\n\n";
 
 ?>
